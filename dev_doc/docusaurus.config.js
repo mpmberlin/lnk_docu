f0ca83387587d569dbcc504e31285096d8245c7d
Marcos Pla Maria <marcos.pla_maria@siemens.com>
2025-07-30 09:07:57 +0200

    a

--- dev_doc/docusaurus.config.js
+++ dev_doc/docusaurus.config.js
@@ -18,7 +18,12 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+   scripts: [
+  {
+    src: 'https://www.googletagmanager.com/gtm.js?id=GTM-KZBNRNWM',
+    async: true,
+  },
+],
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Checklynx', // Usually your GitHub org/user name.
